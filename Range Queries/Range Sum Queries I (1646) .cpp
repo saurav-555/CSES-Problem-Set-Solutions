bbc8d0e9f05d1cfe4cf13a2b066433ebf96e5828
Saurav <57410861+saurav-555@users.noreply.github.com>
2020-05-28 21:54:48 +0530

    Create Range Sum Queries I (1646) .cpp

---
+++ Range Queries/Range Sum Queries I (1646) .cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+using u128 = __uint128_t;
+ 
+#define int long long
+#define mod 1000000007
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, q;
+    cin>>n>>q;
+    int *a = new int[n];
+    for (int i = 0; i < n; ++i) {
+        cin>>a[i];
+    }
+    int *pre = new int [n + 1];
+    pre[0] = 0;
+    for(int i = 1; i<= n; i++){
+        pre[i] = pre[i-1] + a[i-1];
+    }
+    while(q--){
+        int l ,r ;
+        cin>>l>>r;
+        cout<<pre[r] - pre[l -1]<<endl;
+    }
+ 
+}
