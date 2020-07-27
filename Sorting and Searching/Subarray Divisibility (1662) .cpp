86df5ad40f7834b574565b0ff559876b6b5112be
Saurav <57410861+saurav-555@users.noreply.github.com>
2020-07-27 21:44:30 +0530

    Create Subarray Divisibility (1662) .cpp

---
+++ Sorting and Searching/Subarray Divisibility (1662) .cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+using u128 = __uint128_t;
+ 
+#define int long long
+#define idxSet tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define atIdx find_by_order
+#define idxOf order_of_key
+#define MOD 1000000007
+#define MAX 200005
+#define endl '\n'
+ 
+#define v(type) vector<type>
+#define p(type1, type2) pair<type1 , type2>
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+ 
+int dp[MAX];
+ 
+void solve() {
+    int n;
+    cin>>n;
+    vi a(n);
+    for(int i = 0 ; i < n ; i++)
+        cin>>a[i];
+    map<int,int> have;
+    have[0] = 1 ;
+    int c = 0;
+    for(int i = 0 , sum = 0 ; i< n ; i++){
+        sum += a[i];
+        sum %= n;
+        if(sum < 0) sum += n;
+        c += have[sum];
+        have[sum]++;
+    }
+    cout<<c<<endl;
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    //cin>>t;
+    while (t--)
+        solve();
+    return 0;
+}
