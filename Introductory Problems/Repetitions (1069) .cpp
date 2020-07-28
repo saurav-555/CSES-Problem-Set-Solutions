09742811a2d8952fd304eb7b325d3a64efd192ec
Saurav <57410861+saurav-555@users.noreply.github.com>
2020-07-28 14:08:50 +0530

    Create Repetitions (1069) .cpp

---
+++ Introductory Problems/Repetitions (1069) .cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+using u128 = __uint128_t;
+ 
+#define int long long
+#define idxSet tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define atIdx find_by_order
+#define idxOf order_of_key
+#define MOD 1000000007
+#define MAX 200005
+#define endl '\n'
+ 
+#define v(type) vector<type>
+#define p(type1, type2) pair<type1 , type2>
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+ 
+int dp[MAX];
+ 
+void solve() {
+    string x;
+    cin>>x;
+    int maxi = 0;
+    for(int i = 0 ; i < x.size() ; ){
+        int j = i;
+        int c = 0;
+        while(x[i] == x[j]){
+            c++ , j++;
+        }
+        maxi = max(maxi , c);
+        i = j;
+    }
+    cout<<maxi<<endl;
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    //cin>>t;
+    while (t--)
+        solve();
+    return 0;
+}
